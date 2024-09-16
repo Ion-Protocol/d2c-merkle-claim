8dc9d5649a8b0d3282dc77c07c001971baf34b88
Carson <carsonpcase@gmail.com>
2024-09-16 19:24:32 -0400

    package json

--- src/NucleusClaim.sol
+++ src/NucleusClaim.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.21;
 
-import {ERC20} from "solady/tokens/ERC20.sol";
 import {SafeTransferLib} from "solady/utils/SafeTransferLib.sol";
 import {MerkleProofLib} from "solady/utils/MerkleProofLib.sol";
 
