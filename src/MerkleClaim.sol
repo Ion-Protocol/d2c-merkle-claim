bc3646cdd22477b6c13e16d6e2da80ed491d51ac
Carson <carsonpcase@gmail.com>
2024-09-20 10:29:40 -0400

    refactor: natspec comments

--- src/MerkleClaim.sol
+++ src/MerkleClaim.sol
@@ -12,7 +12,7 @@ error INVALID_PARAMS();
 
 /**
  * @title MerkleClaim
- * @notice a claim contract to allow reward distribution based on verified merkle leafs.
+ * @notice A claim contract to allow reward distribution based on verified merkle leafs.
  * @custom:security-contact security@molecularlabs.io
  */
 contract MerkleClaim {
@@ -36,7 +36,7 @@ contract MerkleClaim {
 
 	uint128 constant MIN_PENDING_PERIOD = 1 hours;
 
-    // internal but will have a getter provided
+    // Internal but will have a getter provided.
     bytes32 internal _root;
 
     uint128 public pendingPeriod = MIN_PENDING_PERIOD;
@@ -50,7 +50,7 @@ contract MerkleClaim {
     mapping(address => mapping(address => uint256)) public usersClaimedAmountOfAsset;
 
 	/**
-	* @param _rootRole address to receive the root role permission
+	* @param _rootRole address to receive the root role permission.
 	*/
     constructor(address _rootRole) {
         owner = msg.sender;
@@ -75,7 +75,7 @@ contract MerkleClaim {
     }
 	
 	/**
-	* @dev only owner can unpause the contract
+	* @dev Only owner can unpause the contract.
 	*/
     function unpause() external onlyOwner {
         isPaused = false;
@@ -83,7 +83,7 @@ contract MerkleClaim {
     }
 
 	/**
-	* @dev only hasPauseRole roles can pause the contract
+	* @dev Only hasPauseRole roles can pause the contract.
 	*/
     function pause() external {
         if (hasPauseRole[msg.sender]) {
@@ -95,17 +95,19 @@ contract MerkleClaim {
     }
 
 	/**
-	* @dev owner can adjust pause roles
+	* @dev Owner can adjust pause roles.
+	* @param _pauser address to give pause role to.
+	* @param _hasPauseRole true or false to set address role in the mapping.
 	*/
     function setPauseRole(address _pauser, bool _hasPauseRole) external onlyOwner {
         hasPauseRole[_pauser] = _hasPauseRole;
     }
 
     /**
-     * @dev sets the pending period. This is the time that a newly published
+     * @dev Sets the pending period. This is the time that a newly published
      * root will remain pending before becoming active. This allows the team
-     * time to detect a malicious root push and pause the contract
-     * @param _newPendingPeriod to set in seconds; cannot be less than the MIN_PENDING_PERIOD
+     * time to detect a malicious root push and pause the contract.
+     * @param _newPendingPeriod to set in seconds; cannot be less than the MIN_PENDING_PERIOD.
      */
     function setPendingPeriod(uint128 _newPendingPeriod) external onlyOwner {
 		if(_newPendingPeriod < MIN_PENDING_PERIOD){
@@ -116,8 +118,8 @@ contract MerkleClaim {
     }
 
     /**
-     * @dev Owner may set the root role at will
-     * @param _newRootRole address to set
+     * @dev Owner may set the root role at will.
+     * @param _newRootRole address to set.
      */
     function setRootRole(address _newRootRole) external onlyOwner {
         rootRole = _newRootRole;
@@ -125,10 +127,10 @@ contract MerkleClaim {
     }
 
     /**
-     * @dev Owner may transfer any amount of any asset out of this contract
-     * @param assets array of ERC20s
-     * @param amounts array of amounts to withdraw in line with assets. Must have the same length as assets
-     * @param receiver address of all assets
+     * @dev Owner may transfer any amount of any asset out of this contract.
+     * @param assets array of ERC20s.
+     * @param amounts array of amounts to withdraw in line with assets. Must have the same length as assets.
+     * @param receiver address of all assets.
      */
     function transferAssets(address[] calldata assets, uint256[] calldata amounts, address receiver)
         external
@@ -147,8 +149,8 @@ contract MerkleClaim {
     }
 
     /**
-     * @dev Only Root Role may set the new pending root
-     * @param _newRoot to set to pending
+     * @dev Only Root Role may set the new pending root.
+     * @param _newRoot to set to pending.
      */
     function setPendingRoot(bytes32 _newRoot) external {
         if (msg.sender == rootRole) {
@@ -161,11 +163,11 @@ contract MerkleClaim {
     }
 
     /**
-     * @dev user facing function to call and claim rewards. Can only be called when not paused
-     * @param proof is merkle proof of the leaf and root
-     * @param user to claim rewards for (sent to user)
-     * @param assets array of ERC20 assets
-     * @param totalClaimableForAsset array of total amounts of each asset to claim in order
+     * @dev User facing function to call and claim rewards. Can only be called when not paused.
+     * @param proof is merkle proof of the leaf and root.
+     * @param user to claim rewards for (sent to user).
+     * @param assets array of ERC20 assets.
+     * @param totalClaimableForAsset array of total amounts of each asset to claim in order.
      */
     function claim(
         bytes32[] calldata proof,
@@ -197,7 +199,7 @@ contract MerkleClaim {
     }
 
 	/**
-	* @dev public function to return root, returns pending root instead if the pending period has elapsed
+	* @dev Public function to return root, returns pending root instead if the pending period has elapsed.
 	*/
     function root() public view returns (bytes32) {
         if (pending == _root || block.timestamp < (lastPendingUpdate + pendingPeriod)) {
@@ -207,7 +209,11 @@ contract MerkleClaim {
     }
 
 	/**
-	* @dev helper function to perform validity check in a single line
+	* @dev Helper function to perform validity check in a single line.
+	* @param proof The user provided merkle proof.
+	* @param user The user to receive rewards (part of leaf).
+	* @param assets The array of assets to claim for user.
+	* @param totalClaimableForAsset The array of amounts of each asset to claim.
 	*/
 	function _isValid(
 		bytes32[] calldata proof,
@@ -221,7 +227,7 @@ contract MerkleClaim {
 	}
 
 	/**
-	* @dev helper function to return root, but also switch to the pending root if the pending period has elapsed
+	* @dev Helper function to return root, but also switch to the pending root if the pending period has elapsed.
 	*/
     function _getRoot() internal returns (bytes32) {
         if (block.timestamp < (lastPendingUpdate + pendingPeriod) || pending == bytes32(0)) {
